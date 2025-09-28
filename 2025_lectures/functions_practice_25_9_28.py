ffd1f47940d8bc8aacc12e839f9beda90c37cc67
suxh2019 <suxihong07@gmail.com>
2025-09-28 16:04:44 -0400

    function practice on 9.28

--- 2025_lectures/functions_practice_25_9_28.py
+++ 2025_lectures/functions_practice_25_9_28.py
@@ -2,10 +2,90 @@
 """
 Created on Sun Sep 28 14:26:49 2025
 
+
 @author: xihong
 """
 
+#---------part 1---------------------
+'''
+The syntax for if-else
 
+if condition:
+    # Code to execute if condition is True
+else:
+    # Code to execute if condition is False
+the condition in the if statement is True, the block of code under if is executed;
+ otherwise, the block under else is executed.
+
+'''
+
+a = 4
+b =7
+
+if a > b :
+    print("a is bigger")
+else:
+    print("b is bigger")
+    
+    
+# return the bigger number from two different numbers
+# print which number is bigger
+def return_bigger_number(num1,num2):
+    
+    bigger_number = 0
+    
+    if num1 > num2 :
+        print("num1 is bigger")
+        bigger_number = num1
+        
+    else:
+        print("num2 is bigger")
+        bigger_number = num2
+        
+    return bigger_number
+
+
+num1 = 100
+num2 = 10
+
+bigger_value = return_bigger_number(num1,num2)
+print("The bigger value between", num1,"and", num2,"is",bigger_value)
+
+
+
+'''
+write a function to compare three different numbers
+and return the biggest number
+function name: compare_three_numbers
+hint: use if-else statement
+'''
+def compare_three_numbers(num1,num2,num3):
+    
+    biggest_value = 0
+    
+    
+
+    
+    return biggest_value
+
+
+'''
+#---------------part 2---------------
+
+# How to read and save each element from a list
+
+list_nums = [1,3,6,7,8]
+
+for element in list_nums:
+    print(element) # print each element in the list
+    
+    a = element # save the element value to the variable a 
+    print("Now the value a is ", a)
+'''
+
+
+
+#---------------------Part 3---------------------------
 
 '''
 Write a function to perform  power operation
@@ -13,6 +93,14 @@ result = base ** exponent
 function name: power
 
 '''
+
+
+'''
+write a function to return the absolute value of x.
+function name: absolute
+
+'''
+
 
 
 
@@ -32,11 +120,7 @@ function name: find_max
 ''' 
 
 
-'''
-write a function to return the absolute value of x.
-function name: absolute
 
-'''
 
 x = -10
 base = 2
