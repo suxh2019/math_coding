53ab951d978a11d7559c12d85cd3fab2ed519fe9
suxh2019 <suxihong07@gmail.com>
2025-09-28 19:04:18 -0400

    lec 10.5

---
+++ 2025_lectures/lec_25_10_5.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 28 19:01:58 2025
+
+@author: xihong
+"""
+
+
+
+# How to read and save each element from a list
+
+list_nums = [1,3,6,7,8]
+
+for element in list_nums:
+    print(element) # print each element in the list
+    
+    a = element # save the element value to the variable a 
+    print("Now the value a is ", a)
+
+
+
+
+
+'''
+Write a function to perform  power operation
+result = base ** exponent
+function name: power
+
+'''
+
+
+
+
+
+
+'''
+Write a function to find the minimum value of a list
+function name: find_min
+
+'''
+
+
+
+
+'''
+Write the code to find the maximum value of a list
+function name: find_max
+''' 
+
+
+
+
+x = -10
+base = 2
+exponent =3
+list_num = [1,2,3,4,5,6,7]
+
+'''
+power_value = power(base, exponent)
+min_value = find_min(list)
+max_value = find_max(list)
+
+
+print(base,"to the power ", exponent, "is", power_value)
+print("The minimal value in ",list_num,"is", min_value)
+print("The maximal value in ",list_num,"is", max_value)
+
+'''
