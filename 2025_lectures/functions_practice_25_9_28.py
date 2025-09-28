3d0521362c26094c2d09148bcfe3ed70d8e763d2
suxh2019 <suxihong07@gmail.com>
2025-09-28 14:43:14 -0400

    function practice on 9.28

---
+++ 2025_lectures/functions_practice_25_9_28.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 28 14:26:49 2025
+
+@author: xihong
+"""
+
+
+
+'''
+Write a function to perform  power operation
+result = base ** exponent
+function name: power
+
+'''
+
+
+
+
+'''
+Write a function to find the minimum value of a list
+function name: find_min
+
+'''
+
+
+
+
+'''
+Write the code to find the minimum value of a list
+function name: find_max
+''' 
+
+
+'''
+write a function to return the absolute value of x.
+function name: absolute
+
+'''
+
+x = -10
+base = 2
+exponent =3
+list_num = [1,2,3,4,5,6,7]
+
+'''
+power_value = power(base, exponent)
+min_value = find_min(list)
+max_value = find_max(list)
+abs_value = absolute(x)
+
+
+print(base,"to the power ", exponent, "is", power_value)
+print("The minimal value in ",list_num,"is", min_value)
+print("The maximal value in ",list_num,"is", max_value)
+print("The absolute value of ", x, "is", abs_value)
+
+'''
+
