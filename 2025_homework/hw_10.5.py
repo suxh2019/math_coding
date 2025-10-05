833e71cd2b043f776fe6e101f6d270ca4d8f1a28
suxh2019 <suxihong07@gmail.com>
2025-10-05 19:42:49 -0400

    homework on 10.5

---
+++ 2025_homework/hw_10.5.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Octomber 5,2025
+
+Homework on 10/05
+
+@author: xihong
+"""
+
+
+print()
+print()
+
+
+
+"""
+Write a function to sum the maximum and the minimum of a list
+
+"""
+
+def sum_min_max(list):
+    maximum = -10000
+    minimum =  10000
+    
+    # Read each element from the list and 
+    # Write a for loop to save the smallest value to minimum
+    
+    
+    
+    
+    
+    
+    
+    # Read each element from the list and 
+    # Write a for loop to save the biggest value to maximum
+   
+    
+   
+    
+   
+    
+   
+    sum = maximum + minimum
+    return sum
+
+
+
+
+#Call the function
+list = [-50, 9, 10, 15,25,30,35,40,60,70,100 ]
+value = sum_min_max(list)
+print("The sum of the min and the max is: ", value)
+
+
+
+
+
