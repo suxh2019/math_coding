5e96ce26a3da2cd3085916c6090e4f7207108b42
suxh2019 <suxihong07@gmail.com>
2025-10-06 05:47:40 -0400

    rename files

---
+++ 2025_homework/hw_9.28.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 28 19:05:07 2025
+
+Homework on 9/28
+
+@author: xihong
+"""
+
+'''
+Programming uestion 1:
+Write a function to find the smallest number among three numbers
+Hint:use if-esle
+'''
+def find_min_value(num1,num2,num3):
+    
+    
+    
+    # You need to replace 0 with the variable that holds the smallest value
+    return 0
+
+
+
+
+
+
+
+'''
+Programming question 2:
+Write a function to find the biggest number among four numbers
+Hint: Use if-else
+'''
+def find_max_value(num1,num2,num3,num4):
+    
+    
+    
+    
+    
+    
+    # You need to replace 0 with the variable that holds the max value 
+    return 0  
+
+
+
+# Test your code
+print()
+print()
+
+num1 = 10
+num2 = 200
+num3 = 50
+num4 = 7
+
+
+smallest_value = find_min_value(num1,num2,num3)
+print("The smallest value among three numbers is: ", smallest_value)
+
+
+print()
+max_value = find_max_value(num1,num2,num3,num4)
+print("The max value among four numbers is ", max_value)
+
+
+
+
+
