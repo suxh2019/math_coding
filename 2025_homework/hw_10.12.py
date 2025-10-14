33691c01ff772fe5f5408ef0acce36e9c9476fd6
suxh2019 <suxihong07@gmail.com>
2025-10-14 07:01:11 -0400

    more coding

---
+++ 2025_homework/hw_10.12.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 14 06:46:34 2025
+
+@author: xihong
+"""
+
+
+# write a function to print all values that are greater than small
+# and less than big in the list
+
+'''
+hints: 1) write a for loop to read each value in the list
+       2) for each value, first compare if the value is greater than small and 
+         second compare if the value is less than big; Or you can do the 
+         comparison the other way around
+      3) print values that are greater than small and less than big
+'''
+def filter(list,small,big):
+   
+    
+   
+    
+   
+    
+   
+    
+   
+    
+    # Do not change the return. 
+    return 0 
+
+
+
+
+
+
+
+
+
+
+#Call the function
+list = [-50, 9, 10, 50,15,25,30,35,40,60,70,100,170,2011,300,110 ]
+value = filter(list,50,200)
+
+
