0cc21bab3223d9690db2123fd2f1b5596a7b61e9
suxh2019 <suxihong07@gmail.com>
2025-11-02 16:37:31 -0500

    exam on 11.2

---
+++ 2025_lectures/exam_25.11.2.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 11/1
+Exam for 11/2
+
+@author: xihong
+"""
+
+
+print("-------Question 1-------")
+print()
+
+# Write a for loop to print numbers 1,2,3,4,5
+
+
+
+
+
+
+print()
+print("-------Question 2-------")
+print()
+# write a for loop to print numbers 5,4,3,2,1
+
+
+
+
+print()
+print("-------Question 3-------")
+print()
+# write a for loop to print numbers 1,3,5,7,9
+
+
+
+
+
+print()
+print("-------Question 4-------")
+print()
+
+# write a for loop to print numbers 9,7,5,3,1
+
+
+
+
+
+print()
+print("-------Question 5-------")
+print()
+
+# Given a list of names, write a for loop to print each name from the list
+names = [ "Hermione ","harry", "Albus", "Sirius", "Snape","harry"]
+
+
+
+
+print()
+print("-------Question 6-------")
+print()
+
+# Given a list of names, check if  "harry" is in the list. If "harry" is in the 
+# list, print("harry is found"); otherwise, print("harry is not here.")
+names = [ "Hermione ","harry", "Albus", "Sirius", "Snape","harry"]
+
+
+
+
+print()
+print("-------Question 7-------")
+print()
+
+# Given a list of names, check if  "jessica"  is in the list. If "jessica" is in the 
+# list, print("jessica is found"); otherwise, print("jessica is not here.")
+names = [ "Hermione ","harry", "Albus", "Sirius", "Snape","harry"]
+
+
+
+
+print()
+print("-------Question 8-------")
+print()
+
+# Given a list of names, count how many "harry" in the list and print the number
+names = [ "Hermione ","harry", "Albus", "Sirius", "Snape","harry"]
+
+
+
+print()
+print("-------Question 9-------")
+print()
+
+# Given a list of numbers, write a for loop to print the max value in the list
+numbers = [-4,6,10,66,0,100,10000,20]
+
+
+
+
+print()
+print("-------Question 10-------")
+print()
+
+# Given a list of numbers, write a for loop to count how many values that are 
+# greater than -5 and less than 200, and the print the count number
+numbers = [-4,6,10,66,0,100,10000,20, 30,-10,8]
+
+
+print()
+print("-------Question 11-------")
+print()
+
+# Given a list of numbers, write a for loop to compute the sum of all numbers
+numbers = [1,2,3,4,7,8,12]
+
+
+
+print()
+print("-------Question 12-------")
+print()
+# Given a list of numbers, write a for loop to count how many numbers in the list
+numbers = [1,2,3,4,7,8,12]
+
+
+
+print()
+print("-------Question 13-------")
+print()
+# Given a list of grades, write code to compute the average grade
+# In this case, the average grade is  84. 
+grades = [80,70,75,95,100]
