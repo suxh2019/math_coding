4385805101b891974a5d0d41ca715dead4d39feb
suxh2019 <suxihong07@gmail.com>
2025-12-14 19:08:24 -0500

    homework on 12.14

---
+++ 2025_homework/hw_25.12.14.py.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun 12/14/2025
+
+@author: xihong
+"""
+
+# Question 1:
+# Review lec_25.12.14.py
+
+
+# Question 2:
+# Play with the code line 12 - line 56 and understand how to swap two values
+
+
+# Question 3:
+# Read line 73- line 91 and understand the idea of the Bubble sort algorithm
+
+
+# Question 4:
+# Read line 106- line 123 and understand the idea of the selection sort algorithm
