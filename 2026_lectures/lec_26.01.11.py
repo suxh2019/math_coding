4c298646340ccef588010f549080135cf74b553f
suxh2019 <suxihong07@gmail.com>
2026-01-12 08:54:39 -0500

    lec

---
+++ 2026_lectures/lec_26.01.11.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 12 08:44:50 2026
+
+@author: xihong
+"""
+
+
+# The test on 2026/01/11
+
+'''
+1. Kids take the first written test. 
+   The test can be found math_coding/quiz_test/test_26.01.11.pdf
+   
+2. 29 questions, 60 minutes. 
+
+3. Kids finished the test within 30 minutes
+
+
+'''
