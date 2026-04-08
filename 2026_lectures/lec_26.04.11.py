10568139f2d118848ed24f13c63dbefed483c9fd
suxh2019 <suxihong07@gmail.com>
2026-04-08 08:21:39 -0400

    add examples

--- 2026_lectures/lec_26.04.11.py
+++ 2026_lectures/lec_26.04.11.py
@@ -5,7 +5,46 @@ Created on Tue Apr  7 15:51:33 2026
 Understand how the recursion program works
 
 @author: xihong
+
+
+1. Motivation of using recursion:
+    Recursion helps us solve big problems by breaking them into 
+    smaller versions of the same problem.
+    
+2. Main idea of recursion:
+   Instead of solving everything at once, we say:
+   “Can I solve a smaller version first?”
+   “Then use that answer to solve the bigger one.”
+   
+
+3. Recursion = when something does a smaller version of itself again and 
+   again until it reaches a stopping point.
+
+   Every recursion needs a stopping point (called the base case), or it will go forever!
+4. Daily examples of recursion:
+    (1) Eating a stack of pancakes
+        How do you eat 5 pancakes?
+
+       Eat the top pancake
+       Now you have 4 pancakes left
+       Do the same thing again
+       Stop when no pancakes are left 😄
+
+    (2) Climbing down stairs
+       If you are on stair 5:
+
+       Step down one stair
+       Now you are on stair 4
+       Do the same thing again
+       Keep going until stair 0
+    (3) Mirrors facing each other
+
+      When two mirrors face each other, you see:
+       image inside image inside image...
+       This looks like recursion because the pattern repeats itself.
+
 """
+
 print()
 print("-------Counting Down with Recursion-------------")
 
