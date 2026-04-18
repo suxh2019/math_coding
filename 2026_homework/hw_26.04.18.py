5da7dbfa48b83be35daf3b418f54164ec15cfc27
suxh2019 <suxihong07@gmail.com>
2026-04-18 19:24:27 -0400

    add more questions

--- 2026_homework/hw_26.04.18.py
+++ 2026_homework/hw_26.04.18.py
@@ -4,10 +4,82 @@ Created on Sat Apr 18 15:43:33 2026
 
 @author: xihong
 """
+#--------------------Part 1--Fibonacci numbers and facts--------------------
+'''
+Question 1: what are Fibonacci numbers? Write your answer down below
+
 
 
 '''
-Question 1: write recusrsion code to draw the shape of stars below.
+
+
+#--------------------------------
+'''
+Question 2: The flower Lily has 3 pedals. 3 is a fibonacci number. Can you
+ please write down at least two more flowers that have Fibonacci number pedals?
+
+
+
+'''
+
+#--------------------------------
+'''
+Question 3:  If you look closely at a pinecone, you don’t just see random 
+scales. You can actually trace spirals. There are two spiral directions. 
+Spirals going to the right (clockwise). Spirals going to the left (counterclockwise)
+
+How many scales in the clockwise direction? write down your answer below.
+
+
+
+How many scales in the counterclockwise direction? write down your answer below.
+
+
+'''
+#---------------------------------
+
+'''
+Question 4: A tree follows this rule: it starts with 1 branch
+Each year, branches grow following the pattern: 1, 1, 2, 3, 5, 8, ...
+How many branches are there in Year 7? Write down your answer and show your work.
+
+
+'''
+#---------------------------------
+
+
+
+
+
+'''
+Question 5: Sunflower seeds don’t grow randomly.
+Each new seed appears at the center, grows outward
+and takes a slightly rotated position.So instead of straight rows, 
+seeds naturally form spirals. if you look at a sunflower center:
+seeds form two spiral patterns.
+
+
+What are fibonacci numbers that one spiral direction of sunflower 
+seeds might have?
+
+
+
+What are fibonacci numbers that the other spiral direction of sunflower 
+seeds might have?
+
+
+'''
+#---------------------------------
+
+
+
+
+
+
+
+#-----------------------Part 2---programming assignments----------------
+'''
+Programming problem 1: write recusrsion code to draw the shape of stars below.
 
 
 
@@ -45,7 +117,7 @@ Hint:
 
 
 '''
-Question 2: make some changes to the function stars on the lecture lec_26.04.18.py
+Programming problem 2: make some changes to the function stars on the lecture lec_26.04.18.py
            and output the shape of stars below
 
 ***
