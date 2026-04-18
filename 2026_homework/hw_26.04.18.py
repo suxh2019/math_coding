1de2bab37d5363d291764228615ff6e86a8c0b29
suxh2019 <suxihong07@gmail.com>
2026-04-18 16:04:17 -0400

    homework on 4.18

---
+++ 2026_homework/hw_26.04.18.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 18 15:43:33 2026
+
+@author: xihong
+"""
+
+
+'''
+Question 1: write recusrsion code to draw the shape of stars below.
+
+
+
+*
+**
+***
+****
+*
+**
+***
+****
+*****
+******
+*
+**
+***
+****
+*****
+******
+*******
+********
+
+
+
+Hint:
+     1) review the function stars on the lecture lec_26.04.18.py
+     2) call the function stars three times with different n values
+'''
+
+# Write your code below
+
+
+
+
+
+
+'''
+Question 2: make some changes to the function stars on the lecture lec_26.04.18.py
+           and output the shape of stars below
+
+***
+****
+*****
+******
+*******
+********
+
+
+
+Hint: Change the base case
+      instead of n ==1, set a different number to n
+      
+'''
+
+#Write your code below
+
+
+
+
+
+
