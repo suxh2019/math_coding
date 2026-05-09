908e8ea126b2661f9ebc70c35dad3dd1da11f790
suxh2019 <suxihong07@gmail.com>
2026-05-09 19:02:53 -0400

    homework on May 9

---
+++ 2026_homework/hw_26.05.09.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 16:04:48 2026
+
+@author: xihong
+"""
+
+
+print("----Question 1----")
+
+'''
+what is the keyword continue in Python? 
+What are differences between continue and break?
+
+Search the answers and write them down in English words below.
+
+
+
+'''
+
+print("----Scenario One ----")
+'''
+Scenario one:
+
+    Imaine that you are hungry. There are some foods, such as 🍎 🍌 🥦 🍕. 
+    The rule is to skip broccoli. 
+    How to imitate the eating process using the keyword continue? If the food
+    is brocolli, skip it. Otherwise, eat it.
+    
+    🍎 → eat
+    🍌 → eat
+    🥦 → continue (skip)
+   🍕 → eat
+
+'''
+
+
+print("Question 2: ")
+
+'''
+      
+lights = ["green", "red", "green", "yellow"]
+
+Imagine a self-driving toy car. The rule is to skip the red light. 
+How to imitate the driving process of the toy car? Simiar to the scenario one.
+
+Write down your answer in English below.
+    
+
+
+'''
+
+
+
+
+
+
+
