5e4937dabac10c83620e57bf362d3c193025bef0
suxh2019 <suxihong07@gmail.com>
2026-05-10 08:48:01 -0400

    simple calculator

---
+++ experiments/calculator_if_while.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 10 08:34:43 2026
+
+@author: xihong
+"""
+
+# Store usernames and passwords
+users = {
+    "claire": "1111",
+    "joseph": "2222",
+    "xinwen": "3333",
+    "jennifer": "4444"
+}
+
+print("Welcome to Super Calculator!")
+
+# Login loop
+while True:
+    username = input("Username: ")
+    password = input("Password: ")
+   # Check if username exists
+    if username in users:
+
+        # Check password
+        if users[username] == password:
+            print("Master", username," I am ready to serve you!")
+            break
+
+    print("Wrong username or password. Try again.")
+
+
+print("Type 'bye' anytime to stop.")
+
+while True:
+
+    first = input("Enter first number: ")
+
+    # Stop calculator
+    if first == "bye":
+        print("Goodbye!")
+        break
+
+    second = input("Enter second number: ")
+
+    if second == "bye":
+        print("Goodbye!")
+        break
+
+    # Change text into numbers
+    first = float(first)
+    second = float(second)
+
+    print("Choose an operation:")
+    print("+  Add")
+    print("-  Subtract")
+    print("*  Multiply")
+    print("/  Divide")
+
+    operation = input("Operation: ")
+
+    # Math operations
+    if operation == "+":
+        answer = first + second
+        print("Answer =", answer)
+
+    elif operation == "-":
+        answer = first - second
+        print("Answer =", answer)
+
+    elif operation == "*":
+        answer = first * second
+        print("Answer =", answer)
+
+    elif operation == "/":
+
+        if second == 0:
+            print("Cannot divide by zero!")
+
+        else:
+            answer = first / second
+            print("Answer =", answer)
+
+    else:
+        print("Unknown operation!")
